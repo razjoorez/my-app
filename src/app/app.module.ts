2e5bb874f5787c6a859f7504a6b01816e20b3239
Reza Razjoo <reza.razjoo@yahoo.com>
2024-04-01 15:24:08 +0100

    directive

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,11 +3,13 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
 import { ProductsModule } from './products/products.module';
+import { CopyrightDirective } from './copyright.directive';
 
 
 @NgModule({
   declarations: [
     AppComponent,
+    CopyrightDirective,
   ],
   imports: [
     BrowserModule,
